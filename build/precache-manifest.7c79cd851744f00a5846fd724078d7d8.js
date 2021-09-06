2549bf856ac2067544d1e617b1789dd62acb8b5c
abhishekpanditofficial <abhishekpanditoff@gmail.com>
2021-09-07 03:01:18 +0530

    added build for netlify

---
+++ build/precache-manifest.7c79cd851744f00a5846fd724078d7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e58e52a592c12f5f7f243e65f32615",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a434a06713e6e618013",
+    "url": "/static/css/main.c5eafa85.chunk.css"
+  },
+  {
+    "revision": "bc8b3fd1e79d606368e5",
+    "url": "/static/js/2.f975d92e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f975d92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a434a06713e6e618013",
+    "url": "/static/js/main.f5c87e78.chunk.js"
+  },
+  {
+    "revision": "461bc2b3b87f45cb2e60",
+    "url": "/static/js/runtime-main.40f09c02.js"
+  }
+]);
